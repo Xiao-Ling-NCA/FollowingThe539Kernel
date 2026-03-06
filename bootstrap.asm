5f798acae821aa281ec826391ae166c68db26afd
Chengfu Zhao <143946777+Xiao-Ling-NCA@users.noreply.github.com>
2026-03-06 11:15:51 -0500

    Add files via upload
    Chapter 2 and 3

--- bootstrap.asm
+++ bootstrap.asm
@@ -12,19 +12,31 @@ start:
     jmp 0900h:0000
 
 load_kernel_from_disk:
+    mov ax, [curr_sector_to_load]
+    sub ax, 2
+    mov bx, 512d
+    
+    mul bx
+    mov bx, ax
+
     mov ax, 0900h
     mov es, ax
 
     mov ah, 02h
-    mov al, 01h
+    mov al, 1h
     mov ch, 0h
-    mov cl, 02h
-    mov dh, 0h
-    mov dl, 80h
-    mov bx, 0h
+    mov cl, [curr_sector_to_load]
+    mov dh, 0h 
+    mov dl, 80h 
     int 13h
-    
+
     jc kernel_load_error
+
+    sub byte [number_of_sectors_to_load], 1
+    add byte [curr_sector_to_load], 1
+    cmp byte [number_of_sectors_to_load], 0
+
+    jne load_kernel_from_disk
 
     ret
 
@@ -65,6 +77,9 @@ printing_finished:
 title_string db "The Bootloader of 539kernel",0
 message_string db "The kernel is loading...",0
 load_error_string db "The kernel cannot be loaded",0
+number_of_sectors_to_load db 15d
+curr_sector_to_load db 2d
+
 
 times 510-($-$$) db 0
 dw 0xAA55
